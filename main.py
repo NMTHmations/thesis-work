8f041effee8ff3cffc0f27de4e4508a415825b82
NMTHmations <125398293+NMTHmations@users.noreply.github.com>
2025-04-11 22:40:33 +0200

    Filtered YOLO
    However this Yolo representation is kinda mess

--- main.py
+++ main.py
@@ -14,16 +14,27 @@ while True:
         print("Error: Could not read frame.")
         break
 
-    results = model.infer(frame)[0]
-    detections = sv.Detections.from_inference(results)
+    results = model.infer(frame)[0] # run inference
+    detections = sv.Detections.from_inference(results) # get detections
+    detections = detections[detections["class_name"] == "person"] # filter for person class
+    detections = detections[detections.confidence > 0.85] # filter for confidence > 0.5
+    label_annotator = sv.LabelAnnotator() # get annotations
+    box_annotator = sv.BoxAnnotator() # get boxing
+    labels = [f"{class_name} {confidence:.2f}" for class_name, confidence in zip(detections["class_name"], detections.confidence)]
+    frame = box_annotator.annotate(scene=frame, detections=detections)
+    frame = label_annotator.annotate(scene=frame, detections=detections, labels=labels)
+    
+
+    """
     labels = detections.class_id
     boxes = detections.xyxy
-
     # Display the detections on the frame
+    # I left this commented out in the case of compability issues
     for box, label in zip(boxes, labels):
         x1, y1, x2, y2 = map(int, box)
         cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
         cv2.putText(frame, str(label), (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+    """
 
     cv2.imshow("YOLOv8 Detection", frame)
 
