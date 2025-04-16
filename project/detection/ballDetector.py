dc3cd642153f4db36e0376e8a1f88e4831adfefb
dániel szarka <szarka.danika@gmail.com>
2025-04-16 20:16:25 +0200

    YOLOv11n-based live video processing module

---
+++ project/detection/ballDetector.py
@@ -0,0 +1,85 @@
+from ultralytics import YOLO
+import cv2
+import numpy as np
+
+
+#object detection model
+model = YOLO("yolo11n.pt")
+
+
+def processSingleFrame(frame):
+    """
+        Processes a single frame:
+        - Converts it to grayscale
+        - Runs YOLO to detect sports balls (class ID 32)
+        - Returns both grayscale and detection overlay frames
+    """
+
+    # convert to grayscale
+    graysclafeFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    #class 32 -> sports ball
+    result = model(frame, stream=True, classes=32)
+
+    # end result
+    results = list(result)
+
+    #If any object were detected, draw it on the frame
+    if results:
+        detectedObjects = results[0].plot()
+    else:
+        detectedObjects = frame
+
+    return graysclafeFrame, detectedObjects
+
+def processLiveVideo():
+    """
+        - Captures video from webcam and processes each frame.
+        - Yields original, grayscale, and detection frames.
+    """
+
+    capture = cv2.VideoCapture(0)
+
+    #Alternative testing with a video file
+    #capture = cv2.VideoCapture("../ballpassing.mp4")
+
+    if not capture.isOpened():
+        raise IOError("Cannot capture/open video.")
+
+    try:
+        while True:
+            #Read frames
+            ret, frame = capture.read()
+            if not ret:
+                break
+
+            graysclafeFrame, detectedObjects = processSingleFrame(frame)
+
+            yield frame, graysclafeFrame, detectedObjects
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+    finally:
+        capture.release()
+
+
+
+if __name__ == '__main__':
+
+    """
+        Main loop for running the video processing.
+        - Displays the original, grayscale, and detection output.
+    """
+
+    #for testing purposes
+    for process in processLiveVideo():
+
+        cv2.imshow("live", process[0])
+        cv2.imshow("gray", process[1])
+        cv2.imshow("detect", np.array(process[2]))
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cv2.destroyAllWindows()
