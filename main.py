3f743bf7449ac2321a3ea61e1d404cccd8cd5302
NMTHmations <125398293+NMTHmations@users.noreply.github.com>
2025-07-22 21:05:54 +0200

    speed measurement (unprecise)

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from collections import defaultdict, deque
 import torch
 import supervision as sv
 import cv2
@@ -14,9 +15,20 @@ class PerspectiveTransform:
     
     def apply(self, frame):
         return cv2.warpPerspective(frame, self.matrix, (frame.shape[1], frame.shape[0])) # apply the perspective transformation to the frame
+    
+    def transform_points(self, points: np.ndarray) -> np.ndarray:
+        if points.size == 0:
+            return points
+        
+        reshaped_points = points.reshape(-1,1,2).astype(np.float32)
+        transformed_points = cv2.perspectiveTransform(reshaped_points,self.matrix)
+        return transformed_points.reshape(-1, 2)
+    
+        
 
 model = torch.hub.load('ultralytics/yolov5', 'custom', path='best.pt')
 cap = cv2.VideoCapture("test1.mov")
+
 if not cap.isOpened():
     print("Error: Could not open video.")
     exit()
@@ -44,16 +56,46 @@ target = np.array([
 transformer = PerspectiveTransform(source,target)
 
 tracker = sv.ByteTrack() # create tracker
+label_annotator = sv.LabelAnnotator() # get annotations
+box_annotator = sv.BoxAnnotator() # get boxing
+trace_annotator = sv.TraceAnnotator() # create trace annotator
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model.to(device)
+
+coordinates = defaultdict(lambda: deque(maxlen=int(cap.get(cv2.CAP_PROP_FPS))))
+
+def speedCalculator(points: np.ndarray, detections: sv.Detections):
+    points = transformer.transform_points(points=points).astype(int)
+    
+    speeds = {}
+    
+    # store the transformed coordinates
+    for tracker_id, [_, y] in zip(detections.tracker_id, points):
+        coordinates[tracker_id].append(y)
+
+    for tracker_id in detections.tracker_id:
+    
+        print(len(coordinates[tracker_id]))
+        # wait to have enough data
+        if len(coordinates[tracker_id]) > 0:
+
+            # calculate the speed
+            coordinate_start = coordinates[tracker_id][-1]
+            coordinate_end = coordinates[tracker_id][0]
+            distance = abs(coordinate_start - coordinate_end)
+            time = len(coordinates[tracker_id]) / cap.get(cv2.CAP_PROP_FPS)
+            speed = distance / time * 3.6
+            speeds[tracker_id] = speed
+    
+    return speeds
 
 # TODO: speed measurement
 def detection_frame(frame:tuple):
     # albumentations usage
     blur_image = abm.OneOf([
-        abm.MotionBlur(p=0.5),
-        abm.GaussianBlur(p=0.5)
+        abm.MotionBlur(p=0.6),
+        abm.GaussianBlur(p=0.6)
     ], p=0.4)
     transformed = blur_image(image=frame)
     img = transformed["image"]
@@ -64,10 +106,14 @@ def detection_frame(frame:tuple):
     detections = detections[detections.confidence > 0.35] # filter for confidence > 0.5
     detections = tracker.update_with_detections(detections) # update tracker
     detections = tracker.update_with_detections(detections1) # update tracker
-    label_annotator = sv.LabelAnnotator() # get annotations
-    box_annotator = sv.BoxAnnotator() # get boxing
-    trace_annotator = sv.TraceAnnotator() # create trace annotator
-    labels = [f"{results.names[class_name]} {confidence:.2f}" for class_name, confidence in zip(detections.class_id, detections.confidence)]
+    points = detections.get_anchors_coordinates(anchor=sv.Position.BOTTOM_CENTER)
+    speeds = speedCalculator(points, detections)
+    labels = []
+    for class_id, confidence, tracker_id in zip(detections.class_id, detections.confidence, detections.tracker_id):
+        class_name = results.names[class_id]
+        speed_str = f" | {speeds[tracker_id]:.1f} km/h" if tracker_id in speeds else ""
+        labels.append(f"{class_name} {confidence:.2f}{speed_str}")
+    
     frame = box_annotator.annotate(scene=frame, detections=detections)
     frame = label_annotator.annotate(scene=frame, detections=detections, labels=labels)
     frame = trace_annotator.annotate(scene=frame, detections=detections) # annotate frame with detections
