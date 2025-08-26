6eefe065a93e32359a242bdf772351c4fbe5fe88
szarka dániel <szarka.danika@gmail.com>
2025-08-26 19:25:39 +0200

    updated skeletons, yet to implement

--- project/detection/types/ODModel.py
+++ project/detection/types/ODModel.py
@@ -1,15 +1,19 @@
 from abc import abstractmethod
-
+import supervision as sv
 from inference import get_model, Model
 
 
 class DetectionModel:
     @abstractmethod
-    def infer(self):
+    def infer(self, frame):
         pass
 
     @abstractmethod
-    def batch_infer(self):
+    def batch_infer(self, batch):
+        pass
+
+    @abstractmethod
+    def getDetectionFromResult(self) -> sv.Detections:
         pass
 
 class DetectionModelFactory:
@@ -26,19 +30,33 @@ class YOLOModel(DetectionModel):
 
         self.model = YOLO(modelPath)
 
-    def infer(self):
+    def infer(self, frame):
         pass
 
-    def batch_infer(self):
+    def batch_infer(self, batch):
         pass
 
+    def getDetectionFromResult(self) -> sv.Detections:
+        pass
+
+    def _mapDetectionsToOriginal(self):
+        #From detection thread
+        pass
 
 class RoboflowModel(DetectionModel):
     def __init__(self, modelPath : str, apiKey : str):
         self.model : Model = get_model(modelPath, api_key=apiKey)
 
-    def infer(self):
+    def infer(self, frame):
         pass
 
-    def batch_infer(self):
+    def batch_infer(self, batch):
         pass
+
+    def getDetectionFromResult(self) -> sv.Detections:
+        pass
+
+
+    def _mapDetectionsToOriginal(self):
+        pass
+
