ff706496728faaaab56e61a422baf542f52145d8
szarka dániel <szarka.danika@gmail.com>
2025-08-26 17:14:33 +0200

    changed frame collecting logic

--- project/detection/threads/captureThread.py
+++ project/detection/threads/captureThread.py
@@ -1,29 +1,40 @@
-import queue
 import threading
 import time
+
 import cv2
+
+from project.detection.types.FrameBuffer import FrameBuffer
+from project.detection.types.FrameItem import FrameItem
+from project.detection.types.enums import ThreadNames
 
 
 class CaptureThread(threading.Thread):
-    def __init__(self, stopEvent : threading.Event, source : int | str, frameQueue : queue.Queue):
+    def __init__(self, stopEvent : threading.Event, source : int | str, frameBuffer : FrameBuffer):
         super().__init__()
         self.cap = cv2.VideoCapture(source)
-        self.frameQueue = frameQueue
+        self.frameBuffer : FrameBuffer = frameBuffer
         self.stopEvent = stopEvent
+        self.frameID = 0
 
     def run(self):
+
+        if not self.cap.isOpened():
+            print(f"[{ThreadNames.CAPTURE}] Could not open camera")
+            self.stopEvent.set()
+            return
+
         while not self.stopEvent.is_set():
             ret, frame = self.cap.read()
             if not ret:
+                print(f"[{ThreadNames.CAPTURE}] Could not read frame (end of video or camera error)")
                 break
 
-            if not self.frameQueue.full():
-                self.frameQueue.put(frame)
-
-                #for camera sync, we should save timestamps
-                #self.frameQueue.put((frame,time.time()))
+            self.frameID += 1
+            item = FrameItem(frameID=self.frameID, frame=frame, timestamp=time.time())
+            self.frameBuffer.push(item)
 
         self.cap.release()
+        print(f"[{ThreadNames.CAPTURE}]  Thread stopped.")
 
 
 
