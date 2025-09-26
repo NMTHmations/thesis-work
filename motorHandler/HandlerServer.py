6abb608a239fbf91acecc720842c36dc93fa08cb
NMTHmations <nemeth.bertalan.hunor@gmail.com>
2025-09-27 00:38:15 +0200

    dc motor controller server done!

--- motorHandler/HandlerServer.py
+++ motorHandler/HandlerServer.py
@@ -1,13 +1,23 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from motorController import motorController
 
 app = FastAPI()
 
+motorControll = motorController()
+
 class MotorRevDuration(BaseModel):
-    rev_count: int
+    speed: int
+    direction: bool
+    duration: float
 
 @app.post("/rev/")
 def rev_motor(item: MotorRevDuration):
+    if item.speed > 100:
+        return {
+            "message:": "Speed should not be exceed 100!"
+        }
+    motorControll.moveMotor(item.speed,item.direction,item.duration)
     return {
         "rev duration": item.rev_count
     }
--- motorHandler/motorController.py
+++ motorHandler/motorController.py
@@ -1,36 +1,34 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(16,GPIO.OUT)
-GPIO.setup(18,GPIO.OUT)
-GPIO.setup(22,GPIO.OUT)
-motorSpeed = GPIO.PWM(22,1000)
-motorSpeed.start(0)
+class motorController():
+    def __init__(self):
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(16,GPIO.OUT)
+        GPIO.setup(18,GPIO.OUT)
+        GPIO.setup(22,GPIO.OUT)
+        self.motorSpeed = GPIO.PWM(16,1000)
+        self.motorSpeed.start(0)
 
-def forward(speed):
-    GPIO.output(16,True)
-    GPIO.output(18,False)
-    motorSpeed.ChangeDutyCycle(speed)
+    def _forward(self,speed):
+        GPIO.output(18,True)
+        GPIO.output(22,False)
+        self.motorSpeed.ChangeDutyCycle(speed)
 
-def backward(speed):
-    GPIO.output(16,False)
-    GPIO.output(18,True)
-    motorSpeed.ChangeDutyCycle(speed)
+    def _backward(self,speed):
+        GPIO.output(18,False)
+        GPIO.output(22,True)
+        self.motorSpeed.ChangeDutyCycle(speed)
 
-def stop():
-    GPIO.output(18,False)
-    GPIO.output(16,False)
-    motorSpeed.ChangeDutyCycle(0)
+    def _stop(self):
+        GPIO.output(18,False)
+        GPIO.output(22,False)
+        self.motorSpeed.ChangeDutyCycle(0)
 
-def moveMotor(speed:int,direction:bool,duration:float):
-    if direction == True:
-        forward(speed)
-    else:
-        backward(speed)
-    time.sleep(duration)
-    stop()
-
-#moveMotor(100,False,5)
-while True:
-    backward(100)
+    def moveMotor(self,speed:int,direction:bool,duration:float):
+        if direction == True:
+            self._forward(speed)
+        else:
+            self._backward(speed)
+        time.sleep(duration)
+        self._stop()
