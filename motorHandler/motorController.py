5435438c0f39dc87d6851c78e4fb799fba81735a
NMTHmations <125398293+NMTHmations@users.noreply.github.com>
2025-09-27 22:38:19 +0200

    pin modified

--- motorHandler/motorController.py
+++ motorHandler/motorController.py
@@ -6,23 +6,23 @@ class motorController():
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(16,GPIO.OUT)
         GPIO.setup(18,GPIO.OUT)
-        GPIO.setup(22,GPIO.OUT)
+        GPIO.setup(29,GPIO.OUT)
         self.motorSpeed = GPIO.PWM(16,1000)
         self.motorSpeed.start(0)
 
     def _forward(self,speed):
         GPIO.output(18,True)
-        GPIO.output(22,False)
+        GPIO.output(29,False)
         self.motorSpeed.ChangeDutyCycle(speed)
 
     def _backward(self,speed):
         GPIO.output(18,False)
-        GPIO.output(22,True)
+        GPIO.output(29,True)
         self.motorSpeed.ChangeDutyCycle(speed)
 
     def _stop(self):
         GPIO.output(18,False)
-        GPIO.output(22,False)
+        GPIO.output(29,False)
         self.motorSpeed.ChangeDutyCycle(0)
 
     def moveMotor(self,speed:int,direction:bool,duration:float):
