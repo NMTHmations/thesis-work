3768c934e14d1bbdd8add1402d702e99d83ad6e1
NMTHmations <125398293+NMTHmations@users.noreply.github.com>
2025-09-30 23:35:32 +0200

    minor issue fix

--- motorHandler/RawServer.py
+++ motorHandler/RawServer.py
@@ -3,12 +3,15 @@ from motorController import motorController
 
 controller = motorController()
 INTERFACE = "eth0"
+ETH_TYPE_CUSTOM = b'\x88\xb5'
 s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0003))
 s.bind((INTERFACE, 0))
 
 while True:
     packet, addr = s.recvfrom(65535)
     # Skip Ethernet header (14 bytes)
+    if packet[12:14] != ETH_TYPE_CUSTOM:
+        continue
     payload = packet[14:]
     # strip zero padding
     try:
